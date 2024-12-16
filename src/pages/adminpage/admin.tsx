aeab4b1b8753eb1688f0ddd99dc840435e236857
FataneKorkadbona <korkadbona72@gmail.com>
2024-12-16 12:54:21 +0100

    blå box
    la till en blå box

--- src/pages/adminpage/admin.tsx
+++ src/pages/adminpage/admin.tsx
@@ -1,17 +1,18 @@
-
-
+import styles from './admin.module.scss'
 export default function Admin() {
     return (
-        <div>
-            Admin
+        <>
+            <button className={styles.box}>
+
+            </button>
             {/*
             TODO: Lägga till ta bort Admin rättigheter.
-            TODO: Lösenord för att komma in på sidan.
+            TODO: Finnas Lösenord för att komma in på Admin sidan.
             TODO: Köra Clear.js och ImportEmails.js
             TODO: Ladda upp exce-fil.
             TODO: Manuellt ta bort förslag i efterhand.
             TODO: Ändra footer info.
             */}
-        </div>
+        </>
     );
 }
